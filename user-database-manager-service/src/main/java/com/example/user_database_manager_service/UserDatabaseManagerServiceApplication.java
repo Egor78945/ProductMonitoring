823b88e1a7f695ccaeec348b8b89e6159ed71504
Egor Petrov <egor78396@gmail.com>
2025-07-07 20:05:09 +0300

    user-database-manager-service(init)

---
+++ user-database-manager-service/src/main/java/com/example/user_database_manager_service/UserDatabaseManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.user_database_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDatabaseManagerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDatabaseManagerServiceApplication.class, args);
+    }
+
+}
---
+++ user-database-manager-service/src/test/java/com/example/user_database_manager_service/UserDatabaseManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.user_database_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDatabaseManagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

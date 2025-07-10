ad3124d6ed9d0a4577e6eb211a74483cfc78a450
Egor Petrov <egor78396@gmail.com>
2025-07-10 21:53:59 +0300

    user-database-manager-service(liquibase migration + dockerization)

--- user-database-manager-service/src/main/java/com/example/user_database_manager_service/UserDatabaseManagerServiceApplication.java
+++ user-database-manager-service/src/main/java/com/example/user_database_manager_service/UserDatabaseManagerServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.user_database_manager_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserDatabaseManagerServiceApplication {
 
     public static void main(String[] args) {

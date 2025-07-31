ec9ada1d6a50cec95e5823dd8e88ed734554386f
Egor Petrov <egor78396@gmail.com>
2025-07-31 19:40:33 +0300

    aop-spring-boot-starter(initialized)

---
+++ aop-spring-boot-starter/src/test/java/com/example/aop_spring_boot_starter/AopSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aop_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

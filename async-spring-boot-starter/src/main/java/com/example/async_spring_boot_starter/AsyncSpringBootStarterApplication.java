5e0d651a07644163a3e0b58c4a95bc882266b164
Egor Petrov <egor78396@gmail.com>
2025-08-02 14:27:02 +0300

    async-spring-boot-starter(init)

---
+++ async-spring-boot-starter/src/main/java/com/example/async_spring_boot_starter/AsyncSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.async_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ async-spring-boot-starter/src/test/java/com/example/async_spring_boot_starter/AsyncSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.async_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

2f1745d8a828f0d26609c54bbb689c276fd5625b
Egor Petrov <egor78396@gmail.com>
2025-08-16 16:44:54 +0300

    product-processor-service(load services to router)

--- product-processor-service/src/main/java/com/example/product_processor_service/service/marketplace/initializer/MarketplaceInitializerManager.java
+++ product-processor-service/src/main/java/com/example/product_processor_service/service/marketplace/initializer/MarketplaceInitializerManager.java
@@ -4,10 +4,12 @@ import com.example.product_processor_service.model.marketplace.definition.entity
 import com.example.product_processor_service.repository.marketplace.definition.MarketplaceDefinitionRepository;
 import com.example.product_processor_service.service.marketplace.manager.MarketplaceManagerService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,11 +36,10 @@ public class MarketplaceInitializerManager implements MarketplaceInitializer<Str
         if (marketplaceManagerServiceMap.isEmpty()) {
             for (MarketplaceDefinition marketplaceDefinition : marketplaceDefinitionRepository.findAll()) {
                 List<String> urlList = marketplaceDefinitionRepository.findBaseUrlByMarketplaceId(marketplaceDefinition.getId());
-                System.out.println(urlList);
                 for (String url : urlList) {
                     try {
                         Class<?> managerServiceClass = Class.forName(marketplaceDefinition.getProcessorClassName());
-                        if (managerServiceClass.isAssignableFrom(MarketplaceManagerService.class) && managerServiceClass.isAnnotationPresent(Component.class)) {
+                        if (MarketplaceManagerService.class.isAssignableFrom(managerServiceClass) && AnnotationUtils.findAnnotation(managerServiceClass, Component.class) != null) {
                             MarketplaceManagerService marketplaceManagerService = (MarketplaceManagerService) applicationContext.getBean(managerServiceClass);
                             marketplaceManagerServiceMap.put(url, marketplaceManagerService);
                         }

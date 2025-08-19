11c5b4532c4ce879535acbd3349adf61552b62e6
Egor Petrov <egor78396@gmail.com>
2025-08-19 19:44:51 +0300

    product-processor-service(parser interface)

--- product-processor-service/src/main/java/com/example/product_processor_service/service/marketplace/parser/PyaterochkaMarketplaceDetailsParserManager.java
+++ product-processor-service/src/main/java/com/example/product_processor_service/service/marketplace/parser/PyaterochkaMarketplaceDetailsParserManager.java
@@ -1,13 +1,15 @@
 package com.example.product_processor_service.service.marketplace.parser;
 
-public class PyaterochkaMarketplaceDetailsParserManager implements MarketplaceDetailsParser {
+import com.microsoft.playwright.Page;
+
+public class PyaterochkaMarketplaceDetailsParserManager implements MarketplaceDetailsParser<Page> {
     @Override
-    public String parseName(String url) {
+    public String parseName(Page page) {
         return "";
     }
 
     @Override
-    public int parsePrice(String url) {
+    public int parsePrice(Page page) {
         return 0;
     }
 }

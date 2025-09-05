4b254136cf7f86c9a6eb1fe2af5d9696a53c9ed1
Egor Petrov <egor78396@gmail.com>
2025-09-05 20:31:22 +0300

    product-processor-service(delete useless services)

--- product-processor-service/src/main/java/com/example/product_processor_service/service/web/client/browser/WebBrowserManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.product_processor_service.service.web.client.browser;
-
-import java.net.URI;
-import java.util.Optional;
-
-//TO DELETE
-public interface WebBrowserManager<P> {
-    String loadPage(URI uri);
-}
--- product-processor-service/src/main/java/com/example/product_processor_service/service/web/playwright/browser/PlaywrightWebBrowserContextManager.java
+++
@@ -1,8 +0,0 @@
-package com.example.product_processor_service.service.web.playwright.browser;
-
-import com.example.product_processor_service.service.web.client.browser.WebBrowserManager;
-import com.microsoft.playwright.BrowserContext;
-
-//TO DELETE
-public interface PlaywrightWebBrowserContextManager extends WebBrowserManager<BrowserContext> {
-}

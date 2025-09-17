862fffce7889d1c91e19e41ee1b6a9c73b6d35e6
Egor Petrov <egor78396@gmail.com>
2025-09-17 19:32:46 +0300

    product-manager-service

---
+++ product-manager-service/src/main/kotlin/com/example/product_manager_service/ProductManagerServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.product_manager_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductManagerServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProductManagerServiceApplication>(*args)
+}

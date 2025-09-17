57cac9676460e3daaca4d49a35fe7a9179e05fc1
Egor Petrov <egor78396@gmail.com>
2025-09-17 20:06:15 +0300

    product-manager-service(build.gradle pre config)

--- product-manager-service/build.gradle.kts
+++ product-manager-service/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.5.5"
+    id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
 }
-
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
+
 description = "product-manager-service"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of("17")
     }
 }
 

e1e1be11ba123d5c43aeac6ae34b215222ac1be3
Egor Petrov <egor78396@gmail.com>
2025-09-22 19:00:34 +0300

    starter initialized

---
+++ security-manager-spring-boot-starter/src/main/java/com/example/security_manager_spring_boot_starter/SecurityManagerSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.security_manager_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityManagerSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityManagerSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ security-manager-spring-boot-starter/src/test/java/com/example/security_manager_spring_boot_starter/SecurityManagerSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.security_manager_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityManagerSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

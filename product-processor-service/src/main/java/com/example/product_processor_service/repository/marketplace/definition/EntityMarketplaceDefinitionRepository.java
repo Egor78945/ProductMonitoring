3142d41e30abba1b1f0e7c2a62871a36cd6164ca
Egor Petrov <egor78396@gmail.com>
2025-10-12 18:59:59 +0300

    valid save and looped schedule update products + reformat repositories

--- product-processor-service/src/main/java/com/example/product_processor_service/repository/marketplace/definition/EntityMarketplaceDefinitionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.product_processor_service.repository.marketplace.definition;
-
-import org.jooq.DSLContext;
-
-public abstract class EntityMarketplaceDefinitionRepository extends MarketplaceDefinitionRepository {
-    protected EntityMarketplaceDefinitionRepository(DSLContext dslContext) {
-        super(dslContext);
-    }
-}
--- product-processor-service/src/main/java/com/example/product_processor_service/repository/marketplace/definition/path/EntityMarketplacePathDefinitionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.product_processor_service.repository.marketplace.definition.path;
-
-import org.jooq.DSLContext;
-
-public abstract class EntityMarketplacePathDefinitionRepository extends MarketplacePathDefinitionRepository {
-    public EntityMarketplacePathDefinitionRepository(DSLContext dslContext) {
-        super(dslContext);
-    }
-}

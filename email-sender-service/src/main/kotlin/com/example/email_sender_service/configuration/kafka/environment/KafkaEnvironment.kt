5c3428c81207434e70fe492fbdd991b065118948
Egor Petrov <egor78396@gmail.com>
2025-10-18 18:27:02 +0300

    change group id

--- email-sender-service/src/main/kotlin/com/example/email_sender_service/configuration/kafka/environment/KafkaEnvironment.kt
+++ email-sender-service/src/main/kotlin/com/example/email_sender_service/configuration/kafka/environment/KafkaEnvironment.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 @Component
 class KafkaEnvironment(
     @Value("\${kafka.topic.user.notification.name}") val KAFKA_TOPIC_USER_NOTIFCATION_NAME: String,
-    @Value("\${kafka.topic.user.group-id}") val KAFKA_TOPIC_USER_GROUP_ID: String,
+    @Value("\${spring.kafka.consumer.group-id}") val KAFKA_TOPIC_USER_GROUP_ID: String,
     @Value("\${kafka.replicas}") val KAFKA_REPLICATION_FACTOR: Int,
     @Value("\${spring.kafka.bootstrap-servers}") val KAFKA_BOOTSTRAP_SERVERS: String,
 ) {

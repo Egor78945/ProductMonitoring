39bb961878fc5d8bfab030bda8ee00ae6838d622
Egor Petrov <egor78396@gmail.com>
2025-12-10 13:23:38 +0300

    jwt starter initialized

---
+++ jwt-spring-boot-starter/src/main/java/com/example/jwt_spring_boot_starter/JwtSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwt_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringBootStarterApplication.class, args);
+    }
+
+}
